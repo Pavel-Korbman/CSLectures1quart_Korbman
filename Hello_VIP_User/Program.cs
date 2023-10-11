3cc9d8bf5eda2b8d9bcda443242b2ce167ff4088
Pavel <pavel@b2profit.ru>
2023-10-11 20:31:11 +0300

    Создали программу для особого приветствия Лен и обычного всех остальных

---
+++ Hello_VIP_User/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ваше Имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "лена")
+{
+ Console.Write("УРА!!! Привет, та самая ");
+ Console.Write(username);
+  Console.WriteLine("!");
+}
+else
+{
+Console.Write("Привет! ");
+Console.WriteLine(username);
+}
