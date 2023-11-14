f42b38744882f99667866ac4b1a995ee7b4a7f13
Pavel <pavel@b2profit.ru>
2023-11-14 22:06:50 +0300

    Запись примеров упрощения кода

--- CSLectures4(5)/Program.cs
+++ CSLectures4(5)/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Проверка на чётность. Метод
+
+// int val = Convert.ToInt32(Console.ReadLine());
+
+// bool IsEven (int value) 
+// {
+//     return value % 2 == 0;
+// }
+
+// Console.WriteLine(IsEven(val));
+
+// // Упрощаем:
+
+// int val = Convert.ToInt32(Console.ReadLine());
+
+// bool IsEven (int value) => value % 2 == 0;
+
+// Console.WriteLine(IsEven(val));
+
+// using System.Linq;
+
+// var a=12;
+// Console.WriteLine(a.GetType()); // Печатает разрядностть переменной 
+// напр System.Int32
+// Console.WriteLine(a.GetType().Name); // Печатает разрядностть переменной 
+// // напр Int32
+// var data = new int [] {1,2,3,4}
+// .Where(e=> e>0)
+// .Select(e=> new{q=e, w=e+1});
+
+// Console.WriteLine(data.GetType().Name); // Напечатает WhereSelectArrayIterator`2 
+// // (здесь необходимо было использовать var типизацию вместо int )
+
